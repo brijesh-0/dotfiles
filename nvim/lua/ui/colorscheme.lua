67a351d0d7261554550478809540b251bb765cbb
brijesh <145837789+brijesh-0@users.noreply.github.com>
2025-09-22 18:34:34 +0530

    change: Catppuccin

--- nvim/lua/ui/colorscheme.lua
+++ nvim/lua/ui/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"rebelot/kanagawa.nvim",
-	name = "kanagawa",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd[[colorscheme kanagawa-wave]]
-	end
+		vim.cmd([[colorscheme catppuccin-mocha]])
+	end,
 }
