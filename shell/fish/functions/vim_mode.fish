eb93247cb017687edd4ac4913997dcd5f93ac33d
brijesh <145837789+brijesh-0@users.noreply.github.com>
2026-05-01 13:10:45 +0530

    add(fish): vim and yazi y cmd
    - add vim mode to fish
    - add yazi's y command

--- shell/fish/functions/vim_mode.fish
+++ shell/fish/functions/vim_mode.fish
@@ -1,11 +1,15 @@
 function vim_mode
-	fish_vi_key_bindings --no-erase insert
+    fish_vi_key_bindings --no-erase insert
 
-	bind \cf accept-autosuggestion
-	bind -M insert \cf accept-autosuggestion
+    bind \cf accept-autosuggestion
+    bind -M insert \cf accept-autosuggestion
 
-	bind \cn history-search-forward
-	bind \cp history-search-backward
-	bind -M insert \cn history-search-forward
-	bind -M insert \cp history-search-backward
+    bind \cn history-search-forward
+    bind \cp history-search-backward
+    bind -M insert \cn history-search-forward
+    bind -M insert \cp history-search-backward
+
+    # Vim-like navigation
+    bind -M default H beginning-of-line
+    bind -M default L end-of-line
 end
