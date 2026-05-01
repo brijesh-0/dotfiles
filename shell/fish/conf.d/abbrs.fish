12607ab8d40e403929187f204f3fb479b8bb1df6
brijesh <145837789+brijesh-0@users.noreply.github.com>
2026-05-01 13:08:23 +0530

    remove(fish): clean up plugin configs
    - remove _nvm_ functions
    - remove plugin functions
    - remove completions dir as it can be generated again
    - remove fish_variables to avoid leaking vars

--- shell/fish/conf.d/abbrs.fish
+++ shell/fish/conf.d/abbrs.fish
@@ -8,8 +8,7 @@ abbr -a !! --position anywhere --function last_history_item
 
 abbr -a --position command ftree 'fd -H | tree --fromfile -C -a --dirsfirst'
 
-
-# abbr -a hcat "awk 'FNR==1{if(NR>1)print\"\n======\n\";print FILENAME} {print \$0}'"
+abbr -a cb 'kitten clipboard'
 
 abbr -a daten "date '+%F %T'"
 
@@ -20,7 +19,13 @@ abbr -a --position command n 'nvim'
 abbr -a --position command c 'clear'
 
 # --- Docker ---
-abbr -a docker 'sudo docker'
-abbr -a dcu 'sudo docker compose up -d'
-abbr -a dcd 'sudo docker compose down'
-abbr -a dps 'sudo docker ps -a'
+abbr -a dcu 'docker compose up -d'
+abbr -a dcd 'docker compose down'
+abbr -a dps 'docker ps -a'
+
+# --- Hledger ---
+abbr -a hl 'hledger'
+abbr -a hla 'hledger add'
+abbr -a hlb 'hledger bal not:equity --no-total'
+abbr -a hld 'hledger bal --no-total tag:person --pivot person'
+abbr -a hls 'hledger bal subscriptions tag:company --pivot company'
